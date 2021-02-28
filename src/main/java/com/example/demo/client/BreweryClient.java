a4c296762e4e2475087b3d9341384538e1862a1f
Nick Chen <sakanasan20@gmail.com>
2021-02-28 21:06:14 +0800

    加入 Customer REST Template
    close #6

--- src/main/java/com/example/demo/client/BreweryClient.java
+++ src/main/java/com/example/demo/client/BreweryClient.java
@@ -9,12 +9,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.demo.model.BeerDto;
+import com.example.demo.model.CustomerDto;
 
 @ConfigurationProperties(value = "demo.brewery", ignoreUnknownFields = false)
 @Component
 public class BreweryClient {
 	
 	public final String BEER_PATH_V1 = "/api/v1/beers/";
+	private final String CUSTOMER_PATH_V1 = "/api/v1/customers/";
 	
 	private String apihost;
 	
@@ -45,6 +47,25 @@ public class BreweryClient {
 	public void deleteBeer(UUID beerId) {
 		
 		restTemplate.delete(apihost + BEER_PATH_V1 + beerId.toString());
+	}
+	
+	public CustomerDto getCustomer(UUID customerId) {
+		return restTemplate.getForObject(apihost + CUSTOMER_PATH_V1 + customerId.toString(), CustomerDto.class);
+	}
+
+	public URI postCustomer(CustomerDto customer) {
+		
+		return restTemplate.postForLocation(apihost + CUSTOMER_PATH_V1, customer);
+	}
+	
+	public void putCustomer(UUID customerId, CustomerDto customer) {
+		
+		restTemplate.put(apihost + CUSTOMER_PATH_V1 + customerId.toString(), customer);
+	}
+
+	public void deleteCustomer(UUID customerId) {
+		
+		restTemplate.delete(apihost + CUSTOMER_PATH_V1 + customerId.toString());
 	}
 
 }
--- src/test/java/com/example/demo/client/BreweryClientTest.java
+++ src/test/java/com/example/demo/client/BreweryClientTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.demo.model.BeerDto;
+import com.example.demo.model.CustomerDto;
 
 @SpringBootTest
 class BreweryClientTest {
@@ -54,6 +55,40 @@ class BreweryClientTest {
 	void testDeleteBeer() {
 		
 		breweryClient.deleteBeer(UUID.randomUUID());
+	}
+	
+	@Test
+	void testGetCustomer() {
+		
+		CustomerDto customer = breweryClient.getCustomer(UUID.randomUUID());
+		
+		assertNotNull(customer);
+	}
+	
+	@Test
+	void testPostCustomer() {
+		
+		CustomerDto customer = CustomerDto.builder().name("New Customer").build();
+		
+		URI uri = breweryClient.postCustomer(customer);
+		
+		assertNotNull(uri);
+		
+		System.out.println(uri.toString());
+	}
+	
+	@Test
+	void testPutCustomer() {
+		
+		CustomerDto customer = CustomerDto.builder().name("New Customer").build();
+		
+		breweryClient.putCustomer(UUID.randomUUID() , customer);
+	}
+	
+	@Test
+	void testDeleteCustomer() {
+		
+		breweryClient.deleteCustomer(UUID.randomUUID());
 	}
 
 }
